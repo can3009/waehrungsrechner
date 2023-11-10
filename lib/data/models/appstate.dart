6a3c0f5b897e13c01d63514ee331cdbe5d0fc78c
Can Sakhisada <can_sakhisada@teilnehmer.cdemy.de>
2023-11-10 17:01:54 +0100

    zuhause weiter machen

--- lib/data/models/appstate.dart
+++ lib/data/models/appstate.dart
@@ -1,1 +1,7 @@
+// Klassen erweitern und attribute und methoden
+// todo- app anschauen
+//https://riverpod.dev/docs/providers/state_notifier_provider
+
+// unit test schreiben .
+
 class AppState {}
