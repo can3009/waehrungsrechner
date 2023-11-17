efbd850ed6617c9beb6309c207e46f27709d4bec
can3009 <138504770+can3009@users.noreply.github.com>
2023-11-17 23:50:30 +0100

    martins_safe_dokumente

--- lib/gui/ansicht/home/home_viev.dart
+++ lib/gui/ansicht/home/home_viev.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:waehrungsrechner/logic/app_state_provider.dart';
